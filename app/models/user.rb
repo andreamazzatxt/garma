9c2ff28d7a9549d1e6a464b189523c8da81617e0
andreamazzatxt <andreamazza89@gmail.com>
2021-03-15 17:01:03 +0100

    set destroy dependecies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :garderobe_items, dependent: :destroy # if we remove a user we want to remove also all his saved guardobe_items
   has_many :products, through: :garderobe_items
   validates :first_name, :last_name, :birthday, presence: true
   has_one_attached :photo
