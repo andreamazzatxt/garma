4196d935f56fb196901d9d33f6de88c44d798782
andreamazzatxt <andreamazza89@gmail.com>
2021-03-15 16:54:09 +0100

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :brands, only: [:index] do
+    collection do
+      get ':brand_id/scan/', to: 'brands#scan', as: 'scan'
+      post 'search'
+    end
+  end
+  resources :products, only: [:show] do
+    resources :garderobe_items, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update, :new, :create] do
+    resources :products, only: [:index]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
