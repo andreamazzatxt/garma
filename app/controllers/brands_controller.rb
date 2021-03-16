57f697f7059d144ccc449ece0c6d9daeff22b075
andreamazzatxt <andreamazza89@gmail.com>
2021-03-16 16:25:08 +0100

    implemented scanner

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,9 +1,25 @@
 class BrandsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:scan, :search]
+  skip_before_action :verify_authenticity_token, only: [:search]
   def index
   @brands = Brand.all
   end
 
   def scan
+    @brand_id = params[:id]
+  end
 
+  def search
+    brand_id = params["id"]
+    barcode = params["barcode"]["text"]
+    product = Product.find_by(brand: brand_id, article_number: barcode)
+    p product
+    if product.nil?
+      p "😩 ARTICLE NOT FOUND"
+      render json: { url: nil }
+    else
+      p "✅ ARTICLE FOUND"
+      render json: { url: "/products/#{product.id}" }
+    end
   end
 end
--- app/javascript/plugins/scan.js
+++ app/javascript/plugins/scan.js
@@ -1,42 +1,64 @@
 import { BrowserBarcodeReader } from '@zxing/library';
 
+
+const searchBarcode = (barcode) => {
+  const id = document.getElementById('startButton').dataset.brandId
+  fetch(`/brands/${id}/search`, {
+  method: 'post',
+  body: JSON.stringify({barcode: barcode}),
+  headers: {
+    'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin'
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      if (data.url !== null)
+      {
+        window.location.href = data.url
+      }
+    });
+
+}
+
 const startScanner = () => {
 
-const start = document.getElementById('startButton')
-const reset = document.getElementById('resetButton')
-let selectedDeviceId;
+  const start = document.getElementById('startButton')
+  const reset = document.getElementById('resetButton')
+  let selectedDeviceId;
+  if (start){
+      // Initialize new Scanner
+      const scanner = new BrowserBarcodeReader()
+      scanner.getVideoInputDevices()
+          .then((videoInputDevices) => {
+              selectedDeviceId = videoInputDevices[0].deviceId
+              start.addEventListener('click', () => {
 
-  // Initialize new Scanner
-  const scanner = new BrowserBarcodeReader()
-  scanner.getVideoInputDevices()
-      .then((videoInputDevices) => {
-          selectedDeviceId = videoInputDevices[0].deviceId
-          start.addEventListener('click', () => {
+                  start.classList.add('hide');
+                  reset.classList.remove('hide');
+                  scanner.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
 
-              start.classList.add('hide');
-              reset.classList.remove('hide');
-              scanner.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
-
-                  alert(result);
-                  scanner.reset();
-                  start.classList.remove('hide');
-                  reset.classList.add('hide');
-              }).catch((err) => {
-                // only For testing than pass Error in the console
-                  alert(err);
-                  scanner.reset();
-                  start.classList.remove('hide');
+                      alert(result)
+                      searchBarcode(result);
+                      scanner.reset();
+                      start.classList.remove('hide');
+                      reset.classList.add('hide');
+                  }).catch((err) => {
+                    // only For testing than pass Error in the console
+                      scanner.reset();
+                      start.classList.remove('hide');
+                  })
               })
-          })
 
-          reset.addEventListener('click', () => {
-              scanner.reset();
-          })
+              reset.addEventListener('click', () => {
+                  scanner.reset();
+              })
 
-      })
-      .catch((err) => {
-          alert(err)
-      })
+          })
+          .catch((err) => {
+              console.log(err)
+          })
+        }
 }
 
 export { startScanner }
