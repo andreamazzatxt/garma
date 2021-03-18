706d022c9a5dcb9dfb621f4b4214bf3f5cce6853
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-18 12:30:01 +0100

    make search functional

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -5,10 +5,8 @@ class Brand < ApplicationRecord
 
   include PgSearch::Model
   pg_search_scope :search_by_brand_name,
-    against: [:name],
-    using: {
-      tsearch: { prefix: true }
-    }
+   against: :name,
+      using: {
+        tsearch: { prefix: true }
+      }
 end
-
-
