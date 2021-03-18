a9609abe3fd9485635db72e7b367bcdb95f05b14
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-18 11:44:23 +0100

    display & style search bar

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,7 +2,13 @@ class Brand < ApplicationRecord
   has_many :products
   has_one_attached :photo
   validates :name, :rating, :planet_rating, :people_rating, :animals_rating, :planet_description, :people_description, :animals_description, presence: true
-end
 
+  include PgSearch::Model
+  pg_search_scope :search_by_brand_name,
+    against: [:name],
+    using: {
+      tsearch: { prefix: true }
+    }
+end
 
 
