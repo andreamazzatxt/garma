b1e17111d3961c928c670228f0e4daf85f7c7aa2
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-19 12:17:50 +0100

    admin profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :garderobe_items, dependent: :destroy # if we remove a user we want to remove also all his saved guardobe_items
   has_many :products, through: :garderobe_items
-  validates :first_name, :last_name, :birthday, presence: true
+  validates :first_name, :last_name, :birthday, :gender, :password, presence: true
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
